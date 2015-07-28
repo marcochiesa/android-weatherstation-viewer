5fc0b47bf8e64c71326ef691833614076562f1d0
Marco Chiesa <getmarcochiesa@gmail.com>
2015-07-28 11:59:22 -0500

    Add graph to detail view

--- app/src/main/java/com/getmarco/weatherstationviewer/StationDetailFragment.java
+++ app/src/main/java/com/getmarco/weatherstationviewer/StationDetailFragment.java
@@ -2,6 +2,7 @@ package com.getmarco.weatherstationviewer;
 
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -22,6 +23,7 @@ import android.widget.TextView;
 
 import com.getmarco.weatherstationviewer.data.StationContract;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
@@ -31,7 +33,7 @@ import com.jjoe64.graphview.series.LineGraphSeries;
  * in two-pane mode (on tablets) or a {@link StationDetailActivity}
  * on handsets.
  */
-public class StationDetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+public class StationDetailFragment extends Fragment {
     public static final String LOG_TAG = StationDetailFragment.class.getSimpleName();
     /**
      * Fragment argument representing the (latest) condition URI that this fragment represents.
@@ -42,10 +44,14 @@ public class StationDetailFragment extends Fragment implements LoaderManager.Loa
 
     private ShareActionProvider shareActionProvider;
     private String conditionAtStation;
-    private Uri uri;
+    private Uri latestUri;
     private Cursor latestCursor;
 
-    private static final int DETAIL_LOADER = 0;
+    private LoaderManager.LoaderCallbacks<Cursor> latestLoaderCallbacks = new LatestLoaderCallbacks();
+    private LoaderManager.LoaderCallbacks<Cursor> conditionsLoaderCallbacks = new ConditionsLoaderCallbacks();
+
+    private static final int LATEST_LOADER = 0;
+    private static final int CONDITIONS_LOADER = 1;
 
     private ImageView mIconView;
     private TextView tempView;
@@ -67,7 +73,7 @@ public class StationDetailFragment extends Fragment implements LoaderManager.Loa
                              Bundle savedInstanceState) {
         Bundle arguments = getArguments();
         if (arguments != null) {
-            uri = arguments.getParcelable(StationDetailFragment.DETAIL_URI);
+            latestUri = arguments.getParcelable(StationDetailFragment.DETAIL_URI);
         }
 
         View rootView = inflater.inflate(R.layout.fragment_station_detail, container, false);
@@ -141,84 +147,135 @@ public class StationDetailFragment extends Fragment implements LoaderManager.Loa
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
-        getLoaderManager().initLoader(DETAIL_LOADER, null, this);
+        getLoaderManager().initLoader(LATEST_LOADER, null, latestLoaderCallbacks);
+        getLoaderManager().initLoader(CONDITIONS_LOADER, null, conditionsLoaderCallbacks);
         super.onActivityCreated(savedInstanceState);
     }
 
     void onStationChanged(String newStationTag) {
-        uri = StationContract.ConditionEntry.buildLatestConditionAtStation(newStationTag);
-        getLoaderManager().restartLoader(DETAIL_LOADER, null, this);
+        latestUri = StationContract.ConditionEntry.buildLatestConditionAtStation(newStationTag);
+        getLoaderManager().restartLoader(LATEST_LOADER, null, latestLoaderCallbacks);
+        getLoaderManager().restartLoader(CONDITIONS_LOADER, null, conditionsLoaderCallbacks);
     }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        //Log.v(LOG_TAG, "In onCreateLoader");
-        if (uri == null)
-            return null;
+    private class ConditionsLoaderCallbacks implements LoaderManager.LoaderCallbacks<Cursor> {
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            String stationTag = StationContract.ConditionEntry.getStationFromUri(latestUri);
+            Uri uri = StationContract.ConditionEntry.buildConditionAtStation(stationTag);
 
-        // Now create and return a CursorLoader that will take care of
-        // creating a Cursor for the data being displayed.
-        return new CursorLoader(
-                getActivity(),
-                uri,
-                null,
-                null,
-                null,
-                null
-        );
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-
-        latestCursor = data;
-        if (latestCursor == null || !latestCursor.moveToFirst())
-            return;
-
-        double temp = latestCursor.getDouble(latestCursor.getColumnIndex(StationContract.ConditionEntry.COLUMN_TEMP));
-        String tempString = Utility.formatTemperature(getActivity(), temp);
-        tempView.setText(tempString);
-        tempView.setContentDescription(getString(R.string.a11y_temp, tempString));
-
-        float humidity = latestCursor.getFloat(latestCursor.getColumnIndex(StationContract.ConditionEntry.COLUMN_HUMIDITY));
-        humidityView.setText(getString(R.string.format_humidity, humidity));
-        humidityView.setContentDescription(getString(R.string.a11y_humidity, humidityView.getText()));
-
-        String posLat = latestCursor.getString(latestCursor.getColumnIndex(StationContract.ConditionEntry.COLUMN_LATITUDE));
-        String posLong = latestCursor.getString(latestCursor.getColumnIndex(StationContract.ConditionEntry.COLUMN_LONGITUDE));
-        locationView.setText(posLat + ", " + posLong);
-
-        long date = latestCursor.getLong(latestCursor.getColumnIndex(StationContract.ConditionEntry.COLUMN_DATE));
-        String friendlyDate = Utility.getFriendlyDayString(getActivity(), date);
-        dateView.setText(friendlyDate);
-
-        String name = latestCursor.getString(latestCursor.getColumnIndex(StationContract.StationEntry.COLUMN_NAME));
-        String tag = latestCursor.getString(latestCursor.getColumnIndex(StationContract.StationEntry.COLUMN_TAG));
-        String station = name != null && name.length() > 0 ? name : tag;
-        boolean isMetric = Utility.isMetric(getActivity());
-        String tempUnit = isMetric ? "(C)" : "(F)";
-
-        // for the share intent
-        conditionAtStation = String.format("Station: %s - temp: %s %s, humidity: %s, time: %s",
-                station, tempString, tempUnit, humidityView.getText(), friendlyDate);
-
-        // If onCreateOptionsMenu has already happened, we need to update the share intent now.
-        if (shareActionProvider != null) {
-            shareActionProvider.setShareIntent(createShareConditionIntent());
+            String sortOrder = StationContract.ConditionEntry._ID + " ASC";
+            // Now create and return a CursorLoader that will take care of
+            // creating a Cursor for the data being displayed.
+            return new CursorLoader(
+                    getActivity(),
+                    uri,
+                    null,
+                    null,
+                    null,
+                    sortOrder
+            );
         }
 
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
-        graphView.addSeries(series);
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            LineGraphSeries<DataPoint> tempSeries = new LineGraphSeries<>(new DataPoint[] {});
+            LineGraphSeries<DataPoint> humiditySeries = new LineGraphSeries<>(new DataPoint[] {});
+            int n = 0;
+            while (data.moveToNext()) {
+                double temp = data.getDouble(data.getColumnIndex(StationContract.ConditionEntry.COLUMN_TEMP));
+                double humidity = data.getDouble(data.getColumnIndex(StationContract.ConditionEntry.COLUMN_HUMIDITY));
+                tempSeries.appendData(new DataPoint(n, temp), true, 100);
+                humiditySeries.appendData(new DataPoint(n, humidity), true, 100);
+                n++;
+            }
+
+            tempSeries.setTitle("Temp");
+            tempSeries.setColor(Color.RED);
+            tempSeries.setDrawDataPoints(true);
+            tempSeries.setDataPointsRadius(10);
+            tempSeries.setThickness(8);
+
+            humiditySeries.setTitle("Humidity");
+            humiditySeries.setColor(Color.BLUE);
+            humiditySeries.setDrawDataPoints(true);
+            humiditySeries.setDataPointsRadius(10);
+            humiditySeries.setThickness(8);
+
+            graphView.addSeries(tempSeries);
+            graphView.addSeries(humiditySeries);
+            graphView.setTitle("Conditions");
+            graphView.getLegendRenderer().setVisible(true);
+            graphView.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+        }
     }
 
-    @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
-        latestCursor = null;
+    private class LatestLoaderCallbacks implements LoaderManager.LoaderCallbacks<Cursor> {
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            //Log.v(LOG_TAG, "In onCreateLoader");
+            if (latestUri == null)
+                return null;
+
+            // Now create and return a CursorLoader that will take care of
+            // creating a Cursor for the data being displayed.
+            return new CursorLoader(
+                    getActivity(),
+                    latestUri,
+                    null,
+                    null,
+                    null,
+                    null
+            );
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+            latestCursor = data;
+            if (latestCursor == null || !latestCursor.moveToFirst())
+                return;
+
+            double temp = latestCursor.getDouble(latestCursor.getColumnIndex(StationContract.ConditionEntry.COLUMN_TEMP));
+            String tempString = Utility.formatTemperature(getActivity(), temp);
+            tempView.setText(tempString);
+            tempView.setContentDescription(getString(R.string.a11y_temp, tempString));
+
+            float humidity = latestCursor.getFloat(latestCursor.getColumnIndex(StationContract.ConditionEntry.COLUMN_HUMIDITY));
+            humidityView.setText(getString(R.string.format_humidity, humidity));
+            humidityView.setContentDescription(getString(R.string.a11y_humidity, humidityView.getText()));
+
+            String posLat = latestCursor.getString(latestCursor.getColumnIndex(StationContract.ConditionEntry.COLUMN_LATITUDE));
+            String posLong = latestCursor.getString(latestCursor.getColumnIndex(StationContract.ConditionEntry.COLUMN_LONGITUDE));
+            locationView.setText(posLat + ", " + posLong);
+
+            long date = latestCursor.getLong(latestCursor.getColumnIndex(StationContract.ConditionEntry.COLUMN_DATE));
+            String friendlyDate = Utility.getFriendlyDayString(getActivity(), date);
+            dateView.setText(friendlyDate);
+
+            String name = latestCursor.getString(latestCursor.getColumnIndex(StationContract.StationEntry.COLUMN_NAME));
+            String tag = latestCursor.getString(latestCursor.getColumnIndex(StationContract.StationEntry.COLUMN_TAG));
+            String station = name != null && name.length() > 0 ? name : tag;
+            boolean isMetric = Utility.isMetric(getActivity());
+            String tempUnit = isMetric ? "(C)" : "(F)";
+
+            // for the share intent
+            conditionAtStation = String.format("Station: %s - temp: %s %s, humidity: %s, time: %s",
+                    station, tempString, tempUnit, humidityView.getText(), friendlyDate);
+
+            // If onCreateOptionsMenu has already happened, we need to update the share intent now.
+            if (shareActionProvider != null) {
+                shareActionProvider.setShareIntent(createShareConditionIntent());
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            latestCursor = null;
+        }
     }
 }
