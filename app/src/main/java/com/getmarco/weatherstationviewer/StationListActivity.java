36744648aaca647d2bd2e652938900d98abe7b7e
Marco Chiesa <getmarcochiesa@gmail.com>
2015-07-28 21:18:44 -0500

    Configure for use with Google Cloud Messaging

--- app/src/main/java/com/getmarco/weatherstationviewer/StationListActivity.java
+++ app/src/main/java/com/getmarco/weatherstationviewer/StationListActivity.java
@@ -1,11 +1,23 @@
 package com.getmarco.weatherstationviewer;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.getmarco.weatherstationviewer.gcm.QuickstartPreferences;
+import com.getmarco.weatherstationviewer.gcm.StationRegistrationIntentService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 
 /**
@@ -26,6 +38,10 @@ import android.view.MenuItem;
  */
 public class StationListActivity extends AppCompatActivity
         implements StationListFragment.Callbacks {
+    public static final String LOG_TAG = StationListActivity.class.getSimpleName();
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
 
     private static final String DETAILFRAGMENT_TAG = "DFTAG";
 
@@ -56,8 +72,43 @@ public class StationListActivity extends AppCompatActivity
             getSupportActionBar().setElevation(0f);
         }
 
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+                boolean sentToken = sharedPreferences
+                        .getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false);
+                if (sentToken) {
+                    Log.d(LOG_TAG, getString(R.string.gcm_send_message));
+                } else {
+                    Log.d(LOG_TAG, getString(R.string.token_error_message));
+                }
+            }
+        };
+
+        if (checkPlayServices()) {
+            Log.d(LOG_TAG, getString(R.string.registering_message));
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, StationRegistrationIntentService.class);
+            startService(intent);
+        }
 
         // TODO: If exposing deep links into your app, handle intents here.
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+        checkPlayServices();
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
     }
 
     @Override
@@ -108,5 +159,25 @@ public class StationListActivity extends AppCompatActivity
                     .setData(conditionUri);
             startActivity(detailIntent);
         }
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(LOG_TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
     }
 }
